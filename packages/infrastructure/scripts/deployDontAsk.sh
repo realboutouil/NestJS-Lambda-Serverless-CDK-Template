d6c79f7c3516dcd68c4569ddf170fb4871ec3207
MOHAMMED AMINE BOUTOUIL <boutouilmohammedamine@gmail.com>
2022-06-07 13:24:34 +0100

    fix scripts

--- packages/infrastructure/scripts/deployDontAsk.sh
+++ packages/infrastructure/scripts/deployDontAsk.sh
@@ -5,9 +5,5 @@ set -euo pipefail
 # Deploy the cdk stack
 echo "--- 🚀 Deploying CDK stack..."
 cdk \
-  -c certArn="${CERT_ARN}" \
-  -c domainName="${DOMAIN_NAME}" \
-  -c subDomainName="${SUB_DOMAIN_NAME}" \
-  -c environment="${ENVIRONMENT}" \
   --require-approval never \
   deploy
--- packages/infrastructure/scripts/destroyDontAsk.sh
+++ packages/infrastructure/scripts/destroyDontAsk.sh
@@ -5,8 +5,4 @@ set -euo pipefail
 # Deploy the cdk stack
 echo "--- 🚀 Destroying CDK stack..."
 cdk \
-  -c certArn="${CERT_ARN}" \
-  -c domainName="${DOMAIN_NAME}" \
-  -c subDomainName="${SUB_DOMAIN_NAME}" \
-  -c environment="${ENVIRONMENT}" \
   destroy --all --force
